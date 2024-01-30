1effcd0891243a95d208dea2bdb784aa58a57a78
NuClear1331 <84025789+NuClear1331@users.noreply.github.com>
2024-01-30 14:54:32 +0100

    finalnawersja

--- WinFormsApp5/Form1.Designer.cs
+++ WinFormsApp5/Form1.Designer.cs
@@ -34,12 +34,18 @@
             label1 = new Label();
             pictureBox1 = new PictureBox();
             label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            lowerBoundTextBox = new TextBox();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            upperBoundTextBox = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(175, 251);
+            button1.Location = new Point(177, 310);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 0;
@@ -50,7 +56,7 @@
             // resultLabel
             // 
             resultLabel.AutoSize = true;
-            resultLabel.Location = new Point(162, 215);
+            resultLabel.Location = new Point(177, 347);
             resultLabel.Name = "resultLabel";
             resultLabel.Size = new Size(0, 15);
             resultLabel.TabIndex = 1;
@@ -93,12 +99,66 @@
             label2.TabIndex = 5;
             label2.Text = "Funkcja:";
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(23, 171);
+            label3.Name = "label3";
+            label3.Size = new Size(137, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Podaj liczbę n: (maks 20)";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(23, 189);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 7;
+            // 
+            // lowerBoundTextBox
+            // 
+            lowerBoundTextBox.Location = new Point(23, 232);
+            lowerBoundTextBox.Name = "lowerBoundTextBox";
+            lowerBoundTextBox.Size = new Size(100, 23);
+            lowerBoundTextBox.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(162, 232);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(23, 215);
+            label4.Name = "label4";
+            label4.Size = new Size(80, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Dolna granica";
+            // 
+            // upperBoundTextBox
+            // 
+            upperBoundTextBox.AutoSize = true;
+            upperBoundTextBox.Location = new Point(162, 215);
+            upperBoundTextBox.Name = "upperBoundTextBox";
+            upperBoundTextBox.Size = new Size(81, 15);
+            upperBoundTextBox.TabIndex = 11;
+            upperBoundTextBox.Text = "Górna granica";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.SandyBrown;
             ClientSize = new Size(830, 450);
+            Controls.Add(upperBoundTextBox);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(lowerBoundTextBox);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(pictureBox1);
             Controls.Add(label1);
@@ -120,5 +180,11 @@
         private Label label1;
         private PictureBox pictureBox1;
         private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox lowerBoundTextBox;
+        private TextBox textBox3;
+        private Label label4;
+        private Label upperBoundTextBox;
     }
 }
--- WinFormsApp5/Form1.cs
+++ WinFormsApp5/Form1.cs
@@ -12,6 +12,7 @@ namespace WinFormsApp5
     public partial class Form1 : Form
     {
         private ScottPlot.FormsPlot scottPlot;
+        
         public Form1()
         {
             InitializeComponent();
@@ -20,37 +21,45 @@ namespace WinFormsApp5
             scottPlot.Width = 400;
             scottPlot.Height = 400;
             Controls.Add(scottPlot);
+
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+        
             try
             {
-                
+
                 string functionExpression = functionTextBox.Text;
 
-                
+
                 SymbolicExpression function = SymbolicExpression.Parse(functionExpression);
                 var variableValues = new Dictionary<string, FloatingPoint>
                 {
-                    { "x", 0.0 } 
+                    { "x", 0.0 }
                 };
 
-                
+                double lowerLimit = double.Parse(lowerBoundTextBox.Text);
+                double upperLimit = double.Parse(textBox3.Text);
+
+                double nki_n = double.Parse(textBox1.Text);
+
+               
+                double scaledLowerLimit = ScaleLimits(lowerLimit,lowerLimit,upperLimit);
+                double scaledUpperLimit = ScaleLimits(upperLimit, lowerLimit, upperLimit);
+
+
                 Func<double, double> functionDelegate = x =>
                 {
                     variableValues["x"] = x;
                     return function.Evaluate(variableValues).RealValue;
                 };
 
-                
-                double lowerLimit = -1;
-                double upperLimit = 1;
 
-               
-                double result = Integrate.OnClosedInterval(functionDelegate, lowerLimit, upperLimit, 5);
+                double result = GaussLegendreIntegrationMethod(scaledLowerLimit, scaledUpperLimit, nki_n, functionDelegate);
 
-              
+
                 resultLabel.Text = $"Wynik: {result:F6}";
                 PlotFunction(functionDelegate, lowerLimit, upperLimit);
             }
@@ -61,33 +70,99 @@ namespace WinFormsApp5
         }
         private void PlotFunction(Func<double, double> function, double lower, double upper)
         {
-         
-            var plt = scottPlot.Plot; 
+
+            var plt = scottPlot.Plot;
             plt.Clear();
 
-            
+
             double[] xValues = ScottPlot.DataGen.Range(lower, upper, 0.01);
             double[] yValues = xValues.Select(function).ToArray();
 
-           
+
             plt.PlotScatter(xValues, yValues, label: "Funkcja");
 
-            
+
             plt.PlotVLine(lower, color: System.Drawing.Color.Red, lineStyle: LineStyle.Dash, label: "Granice");
             plt.PlotVLine(upper, color: System.Drawing.Color.Red, lineStyle: LineStyle.Dash);
 
             scottPlot.Refresh();
-            
+
             plt.Legend();
 
-            
+
             plt.Render();
         }
+        static double ScaleLimits(double x, double a, double b)
+        {
+            return ((x - a) / (b - a)) * 2 - 1;
+        }
+        static double GaussLegendreIntegrationMethod(double a, double b, double numPoints, Func<double, double> function)
+        {
+        
+            double[] weights = {
+        0.017614007139152118311861962351852816362143544625,
+        0.040601429800386941331039952274932109879272679893,
+        0.062672048334109063569506535187041606351601848151,
+        0.083276741576704748724758143222046206100285860983,
+        0.101930119817240435036750135480349876166691126300,
+        0.118194531961518417312377377711382287005041219333,
+        0.131688638449176626898494499748163134916110511251,
+        0.142096109318382051329298325067164933034515413392,
+        0.149172986472603746787828737001969436692679111725,
+        0.152753387130725850698084331955097593491948979328,
+        0.152753387130725850698084331955097593491948979328,
+        0.149172986472603746787828737001969436692679111725,
+        0.142096109318382051329298325067164933034515413392,
+        0.131688638449176626898494499748163134916110511251,
+        0.118194531961518417312377377711382287005041219333,
+        0.101930119817240435036750135480349876166691126300,
+        0.083276741576704748724758143222046206100285860983,
+        0.062672048334109063569506535187041606351601848151,
+        0.040601429800386941331039952274932109879272679893,
+        0.017614007139152118311861962351852816362143544625
+    };
+
+            double[] points = {
+        -0.076526521133497333754640409398838211004517335119,
+        -0.227785851141645078080496195368574624743088434007,
+        -0.373706088715419560672548177024927237395746554092,
+        -0.510867001950827098004364050955250998425002199164,
+        -0.636053680726515025452836696226285936743389116804,
+        -0.746331906460150792614305070355641590310730473069,
+        -0.839116971822218823394529061701520685329629365065,
+        -0.912234428251325905867752441203298113049781225802,
+        -0.963971927277913791267666131197277221912060327199,
+        -0.993128599185094924786122388471320278222647147818,
+        0.993128599185094924786122388471320278222647147818,
+        0.963971927277913791267666131197277221912060327199,
+        0.912234428251325905867752441203298113049781225802,
+        0.839116971822218823394529061701520685329629365065,
+        0.746331906460150792614305070355641590310730473069,
+        0.636053680726515025452836696226285936743389116804,
+        0.510867001950827098004364050955250998425002199164,
+        0.373706088715419560672548177024927237395746554092,
+        0.227785851141645078080496195368574624743088434007,
+        0.076526521133497333754640409398838211004517335119
+    };
+            double result = 0;
+
+          
+            for (int i = 0; i < numPoints; i++)
+            {
+                double x = 0.5 * ((b - a) * points[i] + (b + a));
+                result += weights[i] * function(x);
+            }
+
+
+            result *= 0.5 * (b - a);
+
+            return result;
+        }
+
+
 
     }
 
-
-    
 }
 
 
